f92afe89008f635299ba78f89bc944bab722a99c
Max Polonsky <mchp010nsky@gmail.com>
2019-02-15 06:49:06 +0300

    added line and settings of search

---
+++ script/decoration.js
@@ -0,0 +1,26 @@
+'use strict';
+let posSettings = 0;
+
+  $('.b-options-search').click(function () {
+
+    if(posSettings == 0){
+
+      $('.b-options-search img').attr("style", "transform:rotate(-180deg)");
+
+      $('.search-settings').animate({
+        "right": "5px"
+      }, 2000);
+
+      posSettings = 1;
+
+    } else {
+      $('.b-options-search img').attr("style", "transform:rotate(180deg)");
+
+      $('.search-settings').animate({
+        "right": "-280px"
+      }, 2000);
+
+      posSettings = 0;
+    };
+
+  });
--- script/index.js
+++ script/index.js
@@ -1,31 +1,72 @@
+'use strict';
+
 $(document).ready(function(){
   //Code here
-  let key = 'AIzaSyDESXpqfADkPJ0I851gH1BsjIGU57I7m58';
+  const key = 'AIzaSyDESXpqfADkPJ0I851gH1BsjIGU57I7m58';
   let playlistId = 'PLWKjhJtqVAbkyK9woUZUtunToLtNGoQHB';
   let URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+  let searchURL = 'https://www.googleapis.com/youtube/v3/search';
+  let channelURL = 'https://www.googleapis.com/youtube/v3/playlists';
+  //https://www.googleapis.com/youtube/v3/channelSections
+
+  let rangeDisplay = $('#rangeDisplay');
+
+  // buildApiRequest('GET',
+  //               '/youtube/v3/search',
+  //               {'maxResults': '25',
+  //                'part': 'snippet',
+  //                'q': 'surfing',
+  //                'type': ''});
+
+$('#rangeResults').on("input", displayRangeData);
+
+function displayRangeData() {
+  let value = $(this).val();
+  rangeDisplay.prop('textContent', value);
+}
 
   let options = {
     part: 'snippet',
     key: key,
-    maxResults: 20,
-    playlistId: playlistId
+    maxResults: 5,
+    playlistId: playlistId,
+    q: 'how its made',
+    type: ''
+  };
+
+  let optionsChannel={
+    part: 'snippet',
+    channelId: ''
   };
 
 loadVids();
 
   function loadVids() {
-      $.getJSON(URL, options, function(data){
+      $.getJSON(searchURL, options, function(data){
+        console.log(data);
+        // let id = data.items[0].snippet.resourceId.videoId;
+        let id = data.items[0].snippet.channelId;
+        console.log(id);
 
-        let id = data.items[0].snippet.resourceId.videoId;
+        optionsChannel.channelId = `${id}`;
+        console.log(optionsChannel);
 
-        mainVid(id);
+        representChannelPlaylists();
 
-        resultsLoop(data);
+        //mainVid(id);
+
+        //resultsLoop(data);
       })
   };
 
+  function representChannelPlaylists() {
+    $.getJSON(channelURL, optionsChannel, function(channelPlaylists){
+      console.log(channelPlaylists);
+    });
+  };
+
   function mainVid(id) {
-    $('#video').html(`<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allowfullscreen> </iframe>`);
+    $('#video').html(`<iframe width="680" height="385" src="https://www.youtube.com/embed/${id}" frameborder="0" allowfullscreen> </iframe>`);
   };
 
   function resultsLoop(data) {
